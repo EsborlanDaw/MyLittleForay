d134fc1738deac3888518b1dde014879740dca56
Estefania <esborlan.daw@gmail.com>
2022-05-31 16:57:56 +0200

    Acaba la tarjeta JS Datos Reserva, se ha añadido la función 'addPhoto' a JS para incluir la foto en la tarjeta resumen según la elección del ususario

--- js/reserva.js
+++ js/reserva.js
@@ -2,7 +2,7 @@
 
 let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
+  return new bootstrap.Tooltip(tooltipTriggerEl)
 });
 
 //Funcion para ejecutar el toggle
@@ -14,9 +14,9 @@ let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
 
 //Inhabilita las fechas anteriores a hoy
 
-var hoy = new Date().toISOString().split( 'T' )[0];
-document.getElementsByName( "fcheckin" )[0].setAttribute( 'min' , hoy);
-  
+var hoy = new Date().toISOString().split('T')[0];
+document.getElementsByName("fcheckin")[0].setAttribute('min', hoy);
+
 //Funcion para sumar los 7 días a la fecha seleccionada
 
 function sumarDias(frm) {
@@ -27,73 +27,66 @@ function sumarDias(frm) {
   let f = frm.fcheckin.value;
 
   let fecha = new Date(f);
-  fecha.setDate(fecha.getDate () + num);
-  
+  fecha.setDate(fecha.getDate() + num);
+
   //
 
   let mes = fecha.getUTCMonth() + 1;
 
-  if(mes <= 9) 
+  if (mes <= 9)
     mes = '0' + mes;
 
   //
 
   dia = fecha.getUTCDate();
-  if (dia <= 9) 
+  if (dia <= 9)
     dia = '0' + dia;
 
   frm.fcheckout.value = fecha.getUTCFullYear() + '-' + mes + '-' + dia;
-  
+
 }
 
 //Poner imagen
 
-function addPhoto(){
+function addPhoto(e) {
 
-  let pais = document.getElementById("paisdestino").value;
-  let picture = document.createElement("IMG");
+  if (e.keyCode == 13 && !e.shiftKey) {
+    let pais = document.getElementById("paisdestino").value;
+    console.log(pais.toUpperCase());
+    if (pais.toUpperCase() == 'ESPAÑA') {
+      document.getElementById('fotopais').src = "../img/paises/España.jpg";
+    }
 
-  if (pais.toUpperCase() = 'ESPAÑA')
-  {
-    document.getElementById('fotopais').src ="../IMG/paises/España.jpg";
+    else if (pais.toUpperCase() == 'ITALIA') {
+      document.getElementById('fotopais').src = '../img/paises/Italia.jpg';
+    }
+
+    else if ((pais.toUpperCase() == 'JAPÓN') || (pais.toUpperCase() === 'JAPON')) {
+      document.getElementById('fotopais').src = '../img/paises/Japon.jpg';
+    }
+
+    else if (pais.toUpperCase() == 'MEXICO' || (pais.toUpperCase() === 'MÉXICO')) {
+      document.getElementById('fotopais').src = '../img/paises/Mexico.jpg';
+    }
   }
-
-  else if(pais.toUpperCase() = 'ITALIA')
-  {
-    document.getElementById('fotopais').src = '../IMG/paises/Italia.jpg';
-  }
-
-  else if ((pais.toUpperCase() = 'JAPÓN') || (pais.toUpperCase() = 'JAPON'))
-  {
-    document.getElementById('fotopais').src = '../IMG/paises/Japon.jpg';
-  }
-
-  else if (pais.toUpperCase() = 'MEXICO')
-  {
-    document.getElementById('fotopais').src = '../IMG/paises/Mexico.jpg';
-  }
-
-  document.querySelector('.fotopais').appendChild(image);
-  
 }
 
 //Funcion para rellenar Resumen Reserva
-function add()
-{
+function add() {
   let dato = document.getElementById("paisdestino").value;
   document.getElementById("paisconfi").value = dato;
 
-  dato = document.getElementById("ciudad").value;		
-	document.getElementById("ciudadconfi").value = dato;
+  dato = document.getElementById("ciudad").value;
+  document.getElementById("ciudadconfi").value = dato;
 
-  dato = document.getElementById("nom").value;		
-	document.getElementById("nconfi").value = dato;
+  dato = document.getElementById("nom").value;
+  document.getElementById("nconfi").value = dato;
 
-  dato = document.getElementById("tel1").value;		
-	document.getElementById("tconfi").value = dato;
+  dato = document.getElementById("tel1").value;
+  document.getElementById("tconfi").value = dato;
 
-  dato = document.getElementById("email1").value;		
-	document.getElementById("econfi").value = dato;
+  dato = document.getElementById("email1").value;
+  document.getElementById("econfi").value = dato;
 
 }
 
