51e47de67620f7fc620450b5f50354622287b6ef
Luis Rodri <isguezdez@gmail.com>
2022-06-03 16:05:36 +0200

    añadido botones para seleccionar el pais

---
+++ js/paises.js
@@ -0,0 +1,25 @@
+//Creación de los objetos paises con sus atributos
+var españa=new Object();
+españa.titulo = "VEN A ESPAÑA";
+españa.convencer = "Aquí podrás disfrutar de un maravilloso clima mediterraneo y de una comida deliciosa. España es conocida por su diversidad cultural y paisajística, agrupa una gran cantidad de bellos paisajes diferentes en una extensión de terreno no demasiado grande y en la que podrás encontrar en cada región gente distinta. Hay uno para cada viajero. Además, descubrirás grandes y extensas playas de arena blanca o dorada; pequeñas calas escondidas; playas rodeadas de bosques y verde; buenas para bucear, para ir con niños o nudistas… Las hay de todo tipo y para todos los gustos, son muchas las que se pueden visitar y disfrutar durante casi todo el año."
+españa.itinBarc = "BARCELONA";
+españa.itinVal = "VALENCIA";
+españa.itinMal = "MÁLAGA";
+españa.barTxt = "";
+españa.valTxt = "";
+españa.malTxt = "";
+españa.precio = "RESERVA YA A PARTIR DE 450€/p*";
+
+
+//Inserción de texto según el pais
+var italia=new Object();
+italia.titulo = "VEN A ITALIA";
+italia.convencer = "Aquí podrás disfrutar de un maravilloso clima mediterraneo y de una comida deliciosa. España es conocida por su diversidad cultural y paisajística, agrupa una gran cantidad de bellos paisajes diferentes en una extensión de terreno no demasiado grande y en la que podrás encontrar en cada región gente distinta. Hay uno para cada viajero. Además, descubrirás grandes y extensas playas de arena blanca o dorada; pequeñas calas escondidas; playas rodeadas de bosques y verde; buenas para bucear, para ir con niños o nudistas… Las hay de todo tipo y para todos los gustos, son muchas las que se pueden visitar y disfrutar durante casi todo el año."
+italia.itinBarc = "BARCELONA";
+italia.itinVal = "VALENCIA";
+italia.itinMal = "MÁLAGA";
+italia.barTxt = "";
+italia.valTxt = "";
+italia.malTxt = "";
+italia.precio = "RESERVA YA A PARTIR DE 450€/p*";
+
