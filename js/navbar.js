37b95bfc52fe59003e69753d3bca7c27858d1bcc
Estefania <esborlan.daw@gmail.com>
2022-06-06 18:24:56 +0200

    Realizado el navbar con javascript, por Luis

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,8 @@
 var navbar = document.getElementById("navbar");
 var url = window.location.pathname;
-if (url = "/MyLittleForay/index.html") {
+if (url == "/index.html") {
     navbar.innerHTML= '<nav id="barramenu" class="navbar navbar-expand-sm navbar-dark bg-primary"><div class="container-fluid"><button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar"><span>Menú</span><span class="navbar-toggler-icon"></span></button><div class="collapse navbar-collapse" id="collapsibleNavbar"><ul class="navbar-nav" ><li class="nav-item"><a class="nav-link  disabled" href="../index.html">INICIO</a></li><li class="nav-item"><a class="nav-link" href="paises.html">DESTINOS</a></li><li class="nav-item"><a class="nav-link" href="tarjetaregalo.html">TARJETA REGALO</a></li><li class="nav-item"><a class="nav-link" href="../HTML/resenya.html">RESEÑAS</a></li><li class="nav-item"><a class="nav-link" href="../HTML/sobrenosotros.html">SOBRE NOSOTROS</a></li><li class="nav-item"><a class="nav-link" href="../HTML/reserva.html">RESERVAR</a></li></div></ul></div></div></nav>';    
 }
-if (url = "/MyLittleForay/paises/paises.html") {
+if (url == "/paises/paises.html") {
     navbar.innerHTML= '<nav id="barramenu" class="navbar navbar-expand-sm navbar-dark bg-primary"><div class="container-fluid"><button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar"><span>Menú</span><span class="navbar-toggler-icon"></span></button><div class="collapse navbar-collapse" id="collapsibleNavbar"><ul class="navbar-nav" ><li class="nav-item"><a class="nav-link" href="../index.html">INICIO</a></li><li class="nav-item"><a class="nav-link disabled" href="paises.html">DESTINOS</a></li><li class="nav-item"><a class="nav-link" href="tarjetaregalo.html">TARJETA REGALO</a></li><li class="nav-item"><a class="nav-link" href="../HTML/resenya.html">RESEÑAS</a></li><li class="nav-item"><a class="nav-link" href="../HTML/sobrenosotros.html">SOBRE NOSOTROS</a></li><li class="nav-item"><a class="nav-link" href="../HTML/reserva.html">RESERVAR</a></li></div></ul></div></div></nav>';    
 }
