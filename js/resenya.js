caead767b04adf25fe9863b6095231361f5c9fe3
Mario Tomas <mariodaw>
2022-06-08 17:38:35 +0200

    Querido Diario: javascript de about us y resenya acabado, falta rematar el trabajo

---
+++ js/resenya.js
@@ -0,0 +1,26 @@
+let comentario;
+let nota;
+
+let Capturar = function(){
+    let lstNumero = document.getElementsByClassName("form-control"),
+        comentario = [];         
+    for (var i = 0; i < lstNumero.length; i++) {    
+        comentario[i] = lstNumero[i].value;
+    }
+        mostrar(comentario);    
+        document.getElementById("popup").classList.remove("collapse")
+    }
+
+let mostrar = function(comentario){
+    document.getElementById("listado").innerHTML += '<li class="list-group-item d-flex justify-content-between align-items-start" id="'+comentario[0]+'">    <div class="ms-2 me-auto">      <div class="fw-bold">'
+    +comentario[0]+'</div>     '
+    +comentario[1]+'    </div>    <span class="badge bg-primary rounded-pill">'
+    +comentario[2]+'</span>  </li>'
+};
+
+let borrar = function(){
+    document.getElementById(document.getElementsByClassName("form-control")[0].value).outerHTML=""
+};
+let cerraralert = function(){
+    document.getElementById("popup").classList.add("collapse")
+}
--- js/sobre_nosotros.js
+++ js/sobre_nosotros.js
@@ -1,43 +1,17 @@
-$(function(){
-    $('.profile_container').on('click', function(e){
-      let $biginfo = $('#teamcontent');
-      let $bigname = $('#bigname');
-      let $bigjob  = $('#bigjob');
-      let $bigdesc = $('#bigdesc');
-      let $bigimg  = $('.bigimg');  
-      
-      let newimg  = $(this).find('.profilepic').attr('src');
-      let newname = $(this).find('.profilepic').attr('alt');
-      let newrole = $(this).siblings('.job').eq(0).html();
-      let newdesc = $(this).siblings('.desc').eq(0).html();
-      
-      $bigimg.css('background-image', "url(" + newimg + ")");
-      $bigname.html(newname);
-      $bigjob.html(newrole);
-      $bigdesc.html(newdesc);
-      
-      if($biginfo.css('display') == 'none') {
-        $biginfo.slideDown(350);
-      }
-        
-      $('html, body').animate({ scrollTop: $('.team_box').offset().top }, 'slow');  
-    });
-  });
-      
-  $(document).ready(function(){
-    $("a").on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-          
-        let hash = this.hash;
-  
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-     
-          window.location.hash = hash;
-        });
-      } 
-    });
-  });
-  
+let pag1 = function(){
+  document.getElementById("luis").classList.remove("collapse")
+  document.getElementById("fani").classList.add("collapse")
+  document.getElementById("mario").classList.add("collapse")
+};
+
+let pag2 = function(){
+  document.getElementById("luis").classList.add("collapse")
+  document.getElementById("fani").classList.remove("collapse")
+  document.getElementById("mario").classList.add("collapse")
+};
+
+let pag3 = function(){
+  document.getElementById("luis").classList.add("collapse")
+  document.getElementById("fani").classList.add("collapse")
+  document.getElementById("mario").classList.remove("collapse")
+};
