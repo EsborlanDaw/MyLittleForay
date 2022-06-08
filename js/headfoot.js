be429bea54f70c425d8bec3617c8ce56941b7a71
Mario Tomas <mariodaw>
2022-06-08 19:15:36 +0200

    Querido diario: footer y header incrustados con Javascript

---
+++ js/headfoot.js
@@ -0,0 +1,5 @@
+
+document.getElementById("header").innerHTML = '<header> <div class="px-3 py-2 text-center text-white"> <a href="inicio.html"> <img src="../img/IMG/Marca.png" alt="LOGO">   </a> </div> </header>';
+
+document.getElementById("footer").innerHTML = '<div class="footer"> <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">  <div class="col-1" style="width: 30%; text-align: center;">    <h5>Contactanos:</h5>    <ul class="nav flex-column">      <li class="nav-item mb-1"><a href="#" class="nav-link p-0 text-muted">Telefono: 610705043 </a></li>      <li class="nav-item mb-1"><a href="#" class="nav-link p-0 text-muted">Email: mlf@gmail.com</a></li>    </ul>  </div>  <div class="col-md-4 d-flex align-items-center" style="width: 30%; text-align: center;">    <span class="">&copy; 2022 Company, Inc</span>  </div> </footer>'
+
